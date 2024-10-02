337a5bfb2bf586312f7c2a11eff87cec18e9d9d5
tomek <tomekzbroszczyk@gmail.com>
2024-10-02 21:29:36 +0200

    Initial commit

---
+++ src/main/java/uni/projects/talkmeow/TalkMeowApplication.java
@@ -0,0 +1,13 @@
+package uni.projects.talkmeow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkMeowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkMeowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uni/projects/talkmeow/TalkMeowApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.projects.talkmeow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkMeowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
