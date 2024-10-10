685b430c883d7627d7e5ba9732764cd98ca28228
tomek <tomekzbroszczyk@gmail.com>
2024-10-10 13:15:17 +0200

    Manager pages ui

--- src/main/java/uni/projects/talkmeow/components/avatar/Breed.java
+++ src/main/java/uni/projects/talkmeow/components/avatar/Breed.java
@@ -7,7 +7,14 @@ package uni.projects.talkmeow.components.avatar;
  */
 public enum Breed {
     BRITISH_SHORTHAIR,
+    AMERICAN_SHORTHAIR,
     PERSIAN,
     SPHINX,
-    RAGDOLL
+    RAGDOLL,
+    ROOFER,
+    LYNX,
+    CHAUSIE,
+    SYBERIAN,
+    CARTOON
+
 }
