8eea3eaf9706e5d3c2c9aba0376e153448c6b24f
tomek <tomekzbroszczyk@gmail.com>
2024-10-12 13:41:19 +0200

    Added test classes

---
+++ src/test/java/uni/projects/talkmeow/AdminTests.java
@@ -0,0 +1,40 @@
+package uni.projects.talkmeow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uni.projects.talkmeow.Utility.login;
+
+/**
+ * @author Tomasz Zbroszczyk
+ * @version 1.0
+ * @since 12.10.2024
+ */
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class AdminTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final String username = "admin";
+    private final String password = "admin";
+
+    @Test
+    @DisplayName("Admin panel is accessible")
+    void adminPanelIsAccessible() throws Exception {
+        MockHttpSession session = login(username, password, mockMvc);
+           this.mockMvc.perform(get("/admin/home").session(session))
+                   .andExpect(status().isOk());
+    }
+
+}
